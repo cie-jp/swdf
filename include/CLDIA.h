f9762ad829019c590196b4c99af3002eeecdd17d
schwarz <ota@cie.is.t.kanazawa-u.ac.jp>
2018-03-11 22:30:56 +0900

    CLDIA.hを追加した.

---
+++ include/CLDIA.h
@@ -0,0 +1,12 @@
+/***************************************************************** 
+ *
+ * CLDIA (Common Library for Data Integration and Analysis) 
+ *
+ *
+ *                           Created  by Mamoru Ota (2018/03/09)
+ * 
+ *****************************************************************/
+
+#include"FFT.h"
+#include"RANDOM.h"
+#include"SVG/SVGPLOT.h"
