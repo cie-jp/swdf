6147f2c79a9bb226f47b6cc88777eaecbca0a13e
schwarz <ota@cie.is.t.kanazawa-u.ac.jp>
2018-03-23 01:21:51 +0900

    CL31のCDFからデータ(epoch, back_scat, H)を取得するサンプルプログラムを追加した.

--- sample/main.cpp
+++ sample/main.cpp
@@ -6,11 +6,14 @@ using namespace CLDIA;
 int main(){
   TMatrix<DATA> epoch;
   TMatrix<DATA> bscat;
+  TMatrix<DATA> H;
   
   Matrix__fetch(epoch,"epoch"    ,"~/Desktop/ceilo_l1_20170825_v00.cdf");
   Matrix__fetch(bscat,"back_scat","~/Desktop/ceilo_l1_20170825_v00.cdf");
+  Matrix__fetch(H    ,"H"        ,"~/Desktop/ceilo_l1_20170825_v00.cdf");
 
 
   show(epoch | (bscat | 0));
+  show(H);
   return 0;
 }
