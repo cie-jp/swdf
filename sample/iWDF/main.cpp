cf9deb876e4856aae6b584fc4e898dd9ade593bd
Mamoru Ota <ota@pu-toyama.ac.jp>
2018-06-23 12:28:10 +0900

    iWDFの2次元上の3次元データの応用事例の検証プログラムを追加した.

--- sample/iWDF/main.cpp
+++ sample/iWDF/main.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 using namespace CLDIA;
 
-#define N (100)  // データ数
+#define N (1000)  // データ数
 #define L (2)    // xの次元数
-#define K (50)   // モデルパラメータ数
+#define K (10)   // モデルパラメータ数
 #define D (3)    // yの次元数
 
 TMatrix<REAL> Phi(TVector<REAL> &x){
   static INT           isinit = 1;
   static TMatrix<REAL> res(K, D);
   static TVector<REAL> mu [D][K];
-  REAL                 sigma2 = 1.0;
+  REAL                 sigma2 = 2.0;
   
   if(isinit){
     for(int d = 0;d < D;d++){
       for(int k = 0;k < K;k++){
-        mu[d][k] = TVector<REAL>::random(L);
+        mu[d][k] = sqrt(10.0) * TVector<REAL>::random(L);
       }
     }
     isinit = 0;
@@ -33,7 +33,7 @@ TMatrix<REAL> Phi(TVector<REAL> &x){
 int main(int argc,char *argv[]){
   TVector<REAL> x[N];
   TVector<REAL> y[N];
-  TMatrix<REAL> h(K);
+  TVector<REAL> h(K);
   TMatrix<REAL> Lambda(D,D);
   TVector<REAL> m0(K);
   TMatrix<REAL> S0(K,K);
@@ -53,9 +53,31 @@ int main(int argc,char *argv[]){
   TVector<REAL> muN__0(D);
   TVector<REAL> muN0_0(D);
 
+  TVector<REAL> zero(D);
+  TMatrix<REAL> LL  (D,D);
+  TVector<REAL> h0  (K);
+
+  h0[0] = +0.05695;
+  h0[1] = -0.41672;  
+  h0[2] = -2.20679;
+  h0[3] = -0.42250;
+  h0[4] = -1.56381;
+  h0[5] = -0.85653;
+  h0[6] = +0.99113;
+  h0[7] = +0.79933;
+  h0[8] = -0.13248;
+  h0[9] = +1.03572;
+  LL[0][0] = +3.95226;LL[0][1] = +3.14411;LL[0][2] = -0.13404;
+  LL[1][0] = +3.14411;LL[1][1] = +3.72187;LL[1][2] = -1.43830;
+  LL[2][0] = -0.13404;LL[2][1] = -1.43830;LL[2][2] = +2.79577;
+  /*for(int d = 0;d < D;d++){
+    LL[d][d] += 1.0;
+    }*/
+  Multivariate_Normal_Distribution mnd(zero,!LL);
+  
   for(int n = 0;n < N;n++){
     x[n] = TVector<REAL>::random(L);
-    y[n] = TVector<REAL>::random(D);
+    y[n] = ~Phi(x[n]) * h0 + mnd.sampling();//TVector<REAL>::random(D);
   }
 
   /*
@@ -75,6 +97,8 @@ int main(int argc,char *argv[]){
   Lambda = TMatrix<REAL>::random(D,D);
   Lambda = Lambda * ~Lambda;
 
+  Lambda = LL;
+
   /*
     for(int k = 0;k < K;k++){
     m0[k] = 0.0;
@@ -82,15 +106,16 @@ int main(int argc,char *argv[]){
   */
   m0 = TVector<REAL>::random(K);
   
-  /*
-    for(int k = 0;k < K;k++){
+  
+  for(int k = 0;k < K;k++){
     for(int l = 0;l < K;l++){
-    S0[k][l] = (k == l) ? 1.0 : 0.0;
+      S0[k][l] = (k == l) ? 1.0e10 : 0.0;
     }
-    }
-  */
-  S0 = TMatrix<REAL>::random(K,K);
-  S0 = S0 * ~S0;
+  }
+  
+  //S0 = TMatrix<REAL>::random(K,K);
+  //S0 = S0 * ~S0;
+  //S0 *= 1.0e10;
 
   for(int k = 0;k < D;k++){
     for(int l = 0;l < D;l++){
@@ -148,6 +173,19 @@ int main(int argc,char *argv[]){
 
   cerr << (~(y[0] - muN0_0) * !SigmaN0_0 * (y[0] - muN0_0)) << endl;
   cerr << (~(y[0] - muN__0) * !(!Lambda - VN__0) * (y[0] - muN__0)) << endl;
+
+  cerr << (~(y[0] - muN0_0) * !SigmaN0_0 * (y[0] - muN0_0) - ~(y[0] - muN__0) * !SigmaN__0 * (y[0] - muN__0))  << endl;
+  TMatrix<REAL> DN;
+  //DN = !(!(!Lambda - VN__0) - !SigmaN__0);
+  //DN = -(!Lambda - VN__0) * !(!Lambda - VN__0 - SigmaN__0) * SigmaN__0;
+  //DN = 0.5 * (!Lambda - VN__0) * !(VN__0) * SigmaN__0;
+  //DN = 0.5 * !Lambda * !(VN__0) * SigmaN__0 - 0.5 * SigmaN__0;
+  //DN = 0.5 * (SigmaN__0 - VN__0) * !(VN__0) * SigmaN__0 - 0.5 * SigmaN__0;
+  DN = 0.5 * SigmaN__0 * !VN__0 * SigmaN__0 - SigmaN__0;
+  cerr << (~(y[0] - muN__0) * !DN * (y[0] - muN__0)) << endl;
+  cerr << (~(y[0] - muN__0) * (!(!Lambda - VN__0) - !SigmaN__0) * (y[0] - muN__0)) << endl;
+
+  cerr << (h0 | mN) << endl;
   /*
   Multivariate_Normal_Distribution mnd(mN,!AN);  
   
@@ -188,5 +226,6 @@ int main(int argc,char *argv[]){
   cerr << det_lu(A) << endl;
   cerr << det_bidiag(A) << endl;
   */
+  
   return 0;
 }
